78002701af2a26c1d45495d17064903f71aa723e
Alejandro Hernandez <panoramic.giggle@gmail.com>
2021-12-17 20:30:21 -0500

    Hopefully fix deply

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
+  base: "/notes",
+  minify: false,
   plugins: [react()],
   server: { https: !process.env.TESTING },
 })
