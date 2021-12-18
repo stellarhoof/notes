1980b90ef829d278ebb2f5ded0a403e44a8b59d9
Alejandro Hernandez <panoramic.giggle@gmail.com>
2021-12-17 20:51:15 -0500

    Add sourcemaps

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   base: "/notes",
   minify: false,
+  sourcemap: true,
   plugins: [react()],
   server: { https: !process.env.TESTING },
 })
