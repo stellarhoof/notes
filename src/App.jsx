7a326a98ba8e28c4e18f7e20311bdeb68a2927e8
Alejandro Hernandez <panoramic.giggle@gmail.com>
2021-12-20 13:30:46 -0500

    Copy note

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import _ from "lodash/fp"
 import f from "futil"
 import React from "react"
+import striptags from "striptags"
 import { observer } from "mobx-react-lite"
 import {
   Flex,
@@ -11,6 +12,7 @@ import {
   Code,
   Stack,
   Radio,
+  Grid,
   RadioGroup,
   Checkbox,
   CheckboxGroup,
@@ -144,24 +146,9 @@ let ValuePicker = observer(({ store, ...props }) => {
   )
 })
 
-let ReloadFiles = ({ store }) => (
-  <Button
-    size="sm"
-    onClick={async () => {
-      let files = await readFiles({
-        recursive: true,
-        filter: match(/\.(field|temp)$/),
-      })
-      store.setTemplates(files.filter((x) => x.name.endsWith(".temp")))
-      store.setFields(files.filter((x) => x.name.endsWith(".field")))
-    }}
-  >
-    Reload files
-  </Button>
-)
-
 let Note = observer(({ store, ...props }) => (
   <RenderTemplate
+    id="note"
     store={store}
     tree={buildTree(store.getValue)({ id: `${store.path}:1` })}
     renderToken={(props) => (
@@ -171,7 +158,6 @@ let Note = observer(({ store, ...props }) => (
         {...props}
       />
     )}
-    {...props}
   />
 ))
 
@@ -195,24 +181,87 @@ let FilePicker = ({ store }) => (
   </Stack>
 )
 
+let ReloadFilesButton = ({ store, ...props }) => (
+  <Button
+    onClick={async () => {
+      let files = await readFiles({
+        recursive: true,
+        filter: match(/\.(field|temp)$/),
+      })
+      store.setTemplates(files.filter((x) => x.name.endsWith(".temp")))
+      store.setFields(files.filter((x) => x.name.endsWith(".field")))
+    }}
+    {...props}
+  >
+    Reload files
+  </Button>
+)
+
+let CopyNoteButton = (props) => (
+  <Button
+    onClick={() =>
+      navigator.clipboard.writeText(
+        striptags(document.getElementById("note").innerHTML),
+      )
+    }
+    {...props}
+  >
+    Copy Note
+  </Button>
+)
+
+let Section = ({ header, children, Title, ...props }) => (
+  <Stack bg="white" borderRadius="md" boxShadow="md" spacing="0" {...props}>
+    <Flex
+      bg="red.200"
+      p="2"
+      justifyContent="space-between"
+      pos="sticky"
+      top="0"
+    >
+      <Title />
+      {header}
+    </Flex>
+    <Box p="3">{children}</Box>
+  </Stack>
+)
+
+let noteTitle = (store) => observer(() => <b>{store.path || "<- Pick file"}</b>)
+
+let valuePickerTitle = (store) =>
+  observer(() => <b>{pathFromId(store.id) || "<- Pick field"}</b>)
+
 let App = ({ store }) => (
-  <Flex
+  <Grid
     w="100vw"
     h="100vh"
+    p="2"
+    gap="2"
+    bg="gray.100"
     fontFamily="monospace"
-    sx={{
-      "> *:not(hr)": { p: 3, h: "100%", overflow: "scroll" },
-    }}
+    fontSize="sm"
+    gridTemplate={`
+      "file-picker note value-picker" 100%
+      / 20% 40% 40%
+    `}
+    sx={{ "> *": { overflow: "scroll" } }}
   >
-    <Stack flexBasis="20%">
-      <ReloadFiles store={store} />
-      <FilePicker store={store} flex={1} />
-    </Stack>
-    <Note store={store} flexBasis="40%" />
-    <Box flexBasis="40%">
-      <ValuePicker store={store} w="100%" />
-    </Box>
-  </Flex>
+    <Section
+      Title={() => <b>Files</b>}
+      header={<ReloadFilesButton size="xs" colorScheme="red" store={store} />}
+    >
+      <FilePicker store={store} gridArea="file-picker" />
+    </Section>
+    <Section
+      Title={noteTitle(store)}
+      header={<CopyNoteButton size="xs" colorScheme="red" />}
+    >
+      <Note store={store} gridArea="note" />
+    </Section>
+    <Section Title={valuePickerTitle(store)}>
+      <ValuePicker store={store} gridArea="value-picker" />
+    </Section>
+  </Grid>
 )
 
 export default App
